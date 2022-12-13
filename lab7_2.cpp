3dc8e6177a49e78c8bf978b63b67406f906e21fd
Jonut <Natthawatnowan@gmail.com>
2022-12-13 16:55:48 +0700

    LAB_7_2.cpp completed

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -4,11 +4,32 @@ using namespace std;
 
 int main(){
 	
-	int i = 0, P[5000], k, result;
-	while(i < 5000){
+	int i=0;
+    int k,result;
+    int P[5000];
+    while(i < 5000){
 		P[i] = 2*i+1;
 		i++;
 	}
-	
-	return 0;
+
+    cout << "Please input k: " ;
+    cin >> k ;
+
+   
+    if( k > 0){
+        i = 0 , result = 0 ;
+        while (i<5000){
+                if(P[i] % k == 0){
+                    result = result - P[i] ;
+                }else{
+                    result = result + P[i] ;
+                }
+                i = i+1 ;
+            }
+                cout <<"Result = " << result ;
+    }else{
+                cout <<"Invalid input!!!" ;
+    }
+    return 0;
 }
+
